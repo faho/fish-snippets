2f6d25bec6d96fb51f3a7c35fd161fbfe1929d7a
Fabian Boehm <FHomborg@gmail.com>
2026-01-04 12:35:50 +0100

    goblin-mode: Skip read-only fish_terminal_color_theme

--- conf.d/goblin-mode.fish
+++ conf.d/goblin-mode.fish
@@ -3,7 +3,7 @@ function goblin-mode --on-event fish_postexec
     and read -lz all <(random choice (string match -v '*.dat' -- /usr/share/fortune/*))
     and random choice (string split \n%\n -- $all)
 
-    for var in (set -n | string match 'fish_*color*')
+    for var in (set -n | string match 'fish_*color*' | string match -v fish_terminal_color_theme)
         set -g $var (random choice (set_color -c | string match -v black))
     end
 
